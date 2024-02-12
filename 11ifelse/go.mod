304566b094b492d5a0ea39d585dd30cfcbfa53ad
codeRammerG108 <Abhishek.Sandliya@triconinfotech.com>
2024-02-12 15:01:40 +0530

    Section 2: Conditions

---
+++ 11ifelse/go.mod
@@ -0,0 +1,3 @@
+module github.com/codeRammerG108/Ifelse
+
+go 1.22.0
---
+++ 11ifelse/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("If Else Snippet")
+
+	Age := 18
+
+	if Age = Age + 1; Age < 18 {
+		fmt.Println("Below 18")
+	} else {
+		fmt.Println("Welcome to our Finance website!")
+	}
+
+}
