6b112c97cfb21145f93ac7558a4a790fbf6d30b5
codeRammerG108 <Abhishek.Sandliya@triconinfotech.com>
2024-02-13 13:05:00 +0530

    Interface in GO

--- 18inteface/main.go
+++ 18inteface/main.go
@@ -2,52 +2,45 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-// Shape is an interface for geometric shapes
-
-// Rectangle represents a rectangle
-type Rectangle struct {
-	Width  float64
-	Height float64
+type geometry interface {
+	area() float64
+	perim() float64
 }
 
-// Area calculates the area of the rectangle
-func (r Rectangle) Area() float64 {
-	return r.Width * r.Height
+type rect struct {
+	width, height float64
+}
+type circle struct {
+	radius float64
 }
 
-// Perimeter calculates the perimeter of the rectangle
-func (r Rectangle) Perimeter() float64 {
-	return 2*r.Width + 2*r.Height
+func (r rect) area() float64 {
+	return r.width * r.height
+}
+func (r rect) perim() float64 {
+	return 2*r.width + 2*r.height
 }
 
-// Square represents a square
-type Square struct {
-	SideLength float64
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+func (c circle) perim() float64 {
+	return 2 * math.Pi * c.radius
 }
 
-// Area calculates the area of the square
-func (s Square) Area() float64 {
-	return s.SideLength * s.SideLength
-}
-
-// Perimeter calculates the perimeter of the square
-func (s Square) Perimeter() float64 {
-	return 4 * s.SideLength
+func measure(g geometry) {
+	fmt.Println(g)
+	fmt.Println(g.area())
+	fmt.Println(g.perim())
 }
 
 func main() {
-	// Create a rectangle and a square
-	rectangle := Rectangle{Width: 3, Height: 4}
-	square := Square{SideLength: 5}
+	r := rect{width: 3, height: 4}
+	c := circle{radius: 5}
 
-	// Calculate and print area and perimeter of each shape
-	printShapeDetails(rectangle)
-	printShapeDetails(square)
-}
-
-// printShapeDetails prints the area and perimeter of a shape
-func printShapeDetails(shape Shape) {
-	fmt.Printf("Area: %.2f, Perimeter: %.2f\n", shape.Area(), shape.Perimeter())
+	measure(r)
+	measure(c)
 }
